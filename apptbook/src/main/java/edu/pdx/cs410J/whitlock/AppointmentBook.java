6901e8cccccc595519c565c9d4e13edc3d031385
David Whitlock <david.m.whitlock@gmail.com>
2016-06-27 04:21:08 -0700

    Add an implementation of AbstractAppointmentBook that demonstrates how to bind the generic type "T" to a concrete Appointment.

---
+++ apptbook/src/main/java/edu/pdx/cs410J/whitlock/AppointmentBook.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs410J.whitlock;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.Collection;
+
+public class AppointmentBook extends AbstractAppointmentBook<Appointment> {
+  @Override
+  public String getOwnerName() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public Collection<Appointment> getAppointments() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public void addAppointment(Appointment appt) {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+}
