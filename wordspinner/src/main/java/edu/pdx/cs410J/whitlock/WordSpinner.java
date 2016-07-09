9b73af2775360f9deda68ffa63a6cb6382cf0162
David Whitlock <david.m.whitlock@gmail.com>
2016-07-09 16:08:53 -0700

    Implementation of word spinner kata.

--- wordspinner/src/main/java/edu/pdx/cs410J/whitlock/WordSpinner.java
+++ wordspinner/src/main/java/edu/pdx/cs410J/whitlock/WordSpinner.java
@@ -14,4 +14,27 @@ public class WordSpinner {
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
+
+  static String spinWord(String word) {
+    if (word.length() >= 5) {
+      return reverseWord(word);
+
+    } else {
+      return word;
+    }
+  }
+
+  private static String reverseWord(String word) {
+    return new StringBuilder(word).reverse().toString();
+  }
+
+  static String spinSentence(String sentence) {
+    String[] words = sentence.split(" ");
+    StringBuilder sb = new StringBuilder();
+    for (String word : words) {
+      sb.append(spinWord(word));
+      sb.append(" ");
+    }
+    return sb.toString().trim();
+  }
 }
--- wordspinner/src/test/java/edu/pdx/cs410J/whitlock/WordSpinnerTest.java
+++ wordspinner/src/test/java/edu/pdx/cs410J/whitlock/WordSpinnerTest.java
@@ -1,5 +1,10 @@
 package edu.pdx.cs410J.whitlock;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * Unit tests for the Student class.  In addition to the JUnit annotations,
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
@@ -8,5 +13,24 @@ package edu.pdx.cs410J.whitlock;
 public class WordSpinnerTest
 {
 
+  @Test
+  public void spinningWordWithFewerThan5LettersDoesNothing() {
+    String shortWord = "four";
+    assertThat(WordSpinner.spinWord(shortWord), equalTo(shortWord));
+  }
+
+  @Test
+  public void spinningWordWithMoreThan5LettersReversesIt() {
+    String longWord = "camel";
+    assertThat(WordSpinner.spinWord(longWord), equalTo("lemac"));
+  }
+
+  @Test
+  public void spinningASentence() {
+    String sentence = "Hey fellow warriors";
+    String expected = "Hey wollef sroirraw";
+
+    assertThat(WordSpinner.spinSentence(sentence), equalTo(expected));
+  }
 
 }
