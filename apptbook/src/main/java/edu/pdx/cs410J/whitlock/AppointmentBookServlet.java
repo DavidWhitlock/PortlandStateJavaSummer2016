951b028035f238408680441bf9ff3f2408e78317
David Whitlock <david.m.whitlock@gmail.com>
2016-07-20 19:23:58 -0700

    Got the automated tests working again...I hope.

--- apptbook/src/main/java/edu/pdx/cs410J/whitlock/AppointmentBookServlet.java
+++ apptbook/src/main/java/edu/pdx/cs410J/whitlock/AppointmentBookServlet.java
@@ -44,6 +44,11 @@ public class AppointmentBookServlet extends HttpServlet
 
         String owner = getParameter( "owner", request );
 
+        if (owner == null) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         AppointmentBook book = getAppointmentBookForOwner(owner);
 
         prettyPrint(book, response.getWriter());
