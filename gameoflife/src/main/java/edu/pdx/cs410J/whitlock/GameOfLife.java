2dcb214283e842791e0a5178b52e24cd965179b1
David Whitlock <david.m.whitlock@gmail.com>
2016-07-20 20:22:47 -0700

    Added a simple test for the Game of Life.

--- gameoflife/src/main/java/edu/pdx/cs410J/whitlock/GameOfLife.java
+++ gameoflife/src/main/java/edu/pdx/cs410J/whitlock/GameOfLife.java
@@ -4,7 +4,15 @@ package edu.pdx.cs410J.whitlock;
  * This class is represents a <code>Student</code>.                                 
  */                                                                                 
 public class GameOfLife {
-                                                                                    
+
+  private final char[][] grid;
+  private int nextUnpopulatedRow;
+
+  public GameOfLife(int rowCount, int columnCount) {
+    this.grid = new char[rowCount][columnCount];
+    this.nextUnpopulatedRow = 0;
+  }
+
   /**
    * Main program that parses the command line, creates a
    * <code>Student</code>, and prints a description of the gameoflife to
@@ -13,5 +21,19 @@ public class GameOfLife {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public void addRow(String row) {
+    this.grid[this.nextUnpopulatedRow] = row.toCharArray();
+    this.nextUnpopulatedRow++;
+  }
+
+  public void computeNextGeneration() {
+
+
+  }
+
+  public String getRow(int rowIndex) {
+    return new String(this.grid[rowIndex]);
   }
 }
--- gameoflife/src/test/java/edu/pdx/cs410J/whitlock/GameOfLifeTest.java
+++ gameoflife/src/test/java/edu/pdx/cs410J/whitlock/GameOfLifeTest.java
@@ -1,5 +1,10 @@
 package edu.pdx.cs410J.whitlock;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Unit tests for the Student class.  In addition to the JUnit annotations,
  * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
@@ -8,5 +13,12 @@ package edu.pdx.cs410J.whitlock;
 public class GameOfLifeTest
 {
 
+  @Test
+  public void oneDeadCellStaysDead() {
+    GameOfLife game = new GameOfLife(1, 1);
+    game.addRow(".");
+    game.computeNextGeneration();
+    assertThat(game.getRow(0), equalTo("."));
+  }
 
 }
