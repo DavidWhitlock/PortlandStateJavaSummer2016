3272d6a7addb4bbf12cfa7bf52f5c2b1815488dd
David Whitlock <david.m.whitlock@gmail.com>
2016-07-20 20:10:39 -0700

    Added a simple Maven project for the game of life code kata.

---
+++ gameoflife/src/main/java/edu/pdx/cs410J/whitlock/Student.java
@@ -0,0 +1,54 @@
+package edu.pdx.cs410J.whitlock;
+
+import edu.pdx.cs410J.lang.Human;
+
+import java.util.ArrayList;
+                                                                                    
+/**                                                                                 
+ * This class is represents a <code>Student</code>.                                 
+ */                                                                                 
+public class Student extends Human {                                                
+                                                                                    
+  /**                                                                               
+   * Creates a new <code>Student</code>                                             
+   *                                                                                
+   * @param name                                                                    
+   *        The gameoflife's name                                                      
+   * @param classes                                                                 
+   *        The names of the classes the gameoflife is taking.  A gameoflife              
+   *        may take zero or more classes.                                          
+   * @param gpa                                                                     
+   *        The gameoflife's grade point average                                       
+   * @param gender                                                                  
+   *        The gameoflife's gender ("male" or "female", case insensitive)             
+   */                                                                               
+  public Student(String name, ArrayList classes, double gpa, String gender) {
+    super(name);
+  }
+
+  /**                                                                               
+   * All students say "This class is too much work"
+   */
+  @Override
+  public String says() {                                                            
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+                                                                                    
+  /**                                                                               
+   * Returns a <code>String</code> that describes this                              
+   * <code>Student</code>.                                                          
+   */                                                                               
+  public String toString() {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  /**
+   * Main program that parses the command line, creates a
+   * <code>Student</code>, and prints a description of the gameoflife to
+   * standard out by invoking its <code>toString</code> method.
+   */
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+}
