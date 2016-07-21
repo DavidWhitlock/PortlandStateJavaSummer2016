2c63f458199e2e11ca3b71e46f5b509983dfb551
David Whitlock <david.m.whitlock@gmail.com>
2016-07-20 20:12:26 -0700

    Renamed classes for the Game of Life application.

---
+++ gameoflife/src/main/java/edu/pdx/cs410J/whitlock/GameOfLife.java
@@ -0,0 +1,17 @@
+package edu.pdx.cs410J.whitlock;
+
+/**
+ * This class is represents a <code>Student</code>.                                 
+ */                                                                                 
+public class GameOfLife {
+                                                                                    
+  /**
+   * Main program that parses the command line, creates a
+   * <code>Student</code>, and prints a description of the gameoflife to
+   * standard out by invoking its <code>toString</code> method.
+   */
+  public static void main(String[] args) {
+    System.err.println("Missing command line arguments");
+    System.exit(1);
+  }
+}
---
+++ gameoflife/src/test/java/edu/pdx/cs410J/whitlock/GameOfLifeTest.java
@@ -0,0 +1,12 @@
+package edu.pdx.cs410J.whitlock;
+
+/**
+ * Unit tests for the Student class.  In addition to the JUnit annotations,
+ * they also make use of the <a href="http://hamcrest.org/JavaHamcrest/">hamcrest</a>
+ * matchers for more readable assertion statements.
+ */
+public class GameOfLifeTest
+{
+
+
+}
