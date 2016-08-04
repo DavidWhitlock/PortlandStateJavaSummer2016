3af18482d9aacefc2991388a53bce2bc60c5693b
David Whitlock <david.m.whitlock@gmail.com>
2016-08-03 19:19:31 -0700

    Created a GWT project for the Game of Life using the Maven archetype.

---
+++ gameoflife-gwt/src/main/java/edu/pdx/cs410J/whitlock/client/AppointmentBook.java
@@ -0,0 +1,30 @@
+package edu.pdx.cs410J.whitlock.client;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AppointmentBook extends AbstractAppointmentBook<Appointment>
+{
+
+    private Collection<Appointment> appts = new ArrayList<>();
+
+    @Override
+    public String getOwnerName()
+    {
+        return "My Owner";
+    }
+
+    @Override
+    public Collection<Appointment> getAppointments()
+    {
+        return this.appts;
+    }
+
+    @Override
+    public void addAppointment( Appointment appt )
+    {
+        this.appts.add(appt);
+    }
+}
